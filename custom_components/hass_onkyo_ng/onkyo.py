05a48aba263bdefd0aa3c7e2603f363e9d9c6322
Daniel Fairhurst <daniel.fairhurst.82@gmail.com>
2024-03-16 00:27:22 -0400

    Properly parse receiver information and use source list

--- custom_components/hass_onkyo_ng/onkyo.py
+++ custom_components/hass_onkyo_ng/onkyo.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 from collections import defaultdict
 from eiscp.core import Receiver, command_to_iscp, iscp_to_command
+from eiscp.commands import COMMAND_MAPPINGS
 from .const import *
 from .util import dict_merge
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.storage import Store
 import threading
 import logging
-from typing import Any
+from typing import Any, List
 import xml.etree.ElementTree as ET
 
 _LOGGER = logging.getLogger(__name__)
@@ -15,6 +16,73 @@ _ZONE_NAMES = ("main", "zone2", "zone3", "zone4")
 
 class OnkyoReceiver:
     """Class to manage fetching Onkyo data from the receiver."""
+
+    class ReceiverZone:
+        def __init__(self):
+            self.id: int | None = None
+            self.name: str | None = None
+            self.volmax: int | None = None
+            self.sources: List[OnkyoReceiver.ReceiverSource] = []
+
+        def __str__(self):
+            return f"ReceiverZone(id: {self.id}, name: {self.name})"
+
+    class ReceiverSource:
+        def __init__(self):
+            self.id: str | None = None
+            self.name: str | None = None
+            self.zones: List[OnkyoReceiver.ReceiverZone] = []
+
+        def __str__(self):
+            return f"ReceiverSource(id: {self.id}, name: {self.name})"
+
+
+    class ReceiverInfo:
+        def __init__(self):
+            self.model: str | None = None
+            self.serial: str | None = None
+            self.productid: str | None = None
+            self.macaddress: str | None = None
+            self.zones: List[OnkyoReceiver.ReceiverZone] = []
+            self.sources: List[OnkyoReceiver.ReceiverSource] = []
+
+        def __str__(self):
+            return f"ReceiverInfo(model: {self.model}, serial: {self.serial}, productid: {productid}, macaddress: {macaddress}, zones: {self.zones}, sources: {self.sources})"
+
+        def parse_xml(self, receiver_information_xml: str):
+            data = ET.fromstring(receiver_information_xml)
+            device = data.find('device')
+            self.model = device.find('model').text
+            self.serial = device.find('deviceserial').text
+            self.productid = device.find('productid').text
+            self.macaddress = device.find('macaddress').text
+
+            zone_map = {}
+            self.zones = []
+            for zone in device.find('zonelist').findall('zone'):
+                if int(zone.attrib['value']) > 0:
+                    receiver_zone = OnkyoReceiver.ReceiverZone()
+                    receiver_zone.id = int(zone.attrib['id'], 16)
+                    receiver_zone.name = zone.attrib['name'].lower()
+                    receiver_zone.volmax = int(zone.attrib['volmax'])
+
+                    zone_map[receiver_zone.id] = receiver_zone
+                    self.zones.append(receiver_zone)
+
+            self.sources = []
+            for source in device.find('selectorlist').findall('selector'):
+                if int(source.attrib['value']) > 0:
+                    receiver_source = OnkyoReceiver.ReceiverSource()
+                    receiver_source.id = int(source.attrib['id'], 16)
+                    receiver_source.name = source.attrib['name']
+
+                    # Assume this is a bitwise identifier for which zones support this source
+                    source_zones = int(source.attrib['zone'], 16)
+                    for zone_id in zone_map.keys():
+                        if source_zones & (1 << (zone_id - 1)):
+                            receiver_source.zones.append(zone_map[zone_id])
+                            zone_map[zone_id].sources.append(receiver_source)
+                    self.sources.append(receiver_source)
 
     def __init__(
         self,
@@ -63,12 +131,19 @@ class OnkyoReceiver:
                 ATTR_SOUND_MODE: None,
             }
 
-        # Perform synchronously to ensure we have basic data
-        self.command_sync('dock.receiver-information=query')
+    @property
+    def receiver_info(self) -> OnkyoReceiver.ReceiverInfo:
+        if not self._receiver_info:
+            self.command_sync('dock.receiver-information=query')
+        return self._receiver_info
 
     @property
-    def zones(self):
-        return self._receiver_info['zones']
+    def zones(self) -> List[OnkyoReceiver.ReceiverZone]:
+        return self.receiver_info.zones
+
+    @property
+    def sources(self) -> List[OnkyoReceiver.ReceiverSource]:
+        return self.receiver_info.sources
 
     async def load_data(self):
         if self._storage:
@@ -111,7 +186,7 @@ class OnkyoReceiver:
         updates = defaultdict(dict)
         try:
             message_decoded = iscp_to_command(message, with_zone=True)
-            _LOGGER.info(f"Received command: {message_decoded}")
+            _LOGGER.debug(f"Received command: {message_decoded}")
             zone, command, attrib = message_decoded
             if zone in _ZONE_NAMES:
                 zone_key = f"{ATTR_ZONE}_{zone}"
@@ -128,14 +203,8 @@ class OnkyoReceiver:
                 elif command in ("master-volume", "volume"):
                     updates[zone_key][ATTR_VOLUME] = attrib / (self._receiver_max_volume * self._max_volume / 100)
                 elif command in ["input-selector", "selector"]:
-                    sources = self._parse_onkyo_payload((command, attrib))
-                    source = "_".join(sources)
-                    if not source in self._reverse_source_mapping:
-                        # New source found
-                        self._reverse_source_mapping[source] = sources[0]
-                        updates[ATTR_SOURCES] = list(self._reverse_source_mapping.keys())
-                        self.store_data()
-                    updates[zone_key][ATTR_SOURCE] = source
+                    source_id = int(message[-2:], 16)
+                    updates[zone_key][ATTR_SOURCE] = source_id
                 elif command == "preset":
                     updates[zone_key][ATTR_PRESET] = attrib
                 elif command == "hdmi-output-selector":
@@ -152,13 +221,16 @@ class OnkyoReceiver:
                     updates[zone_key][ATTR_SOUND_MODE] = sound_mode
             elif zone == 'dock':
                 if command == "receiver-information":
-                    _LOGGER.info("Got receiver info. Parsing")
-                    info = self._parse_receiver_information(attrib)
+                    _LOGGER.debug("Got receiver info. Parsing")
+                    info = OnkyoReceiver.ReceiverInfo()
+                    info.parse_xml(attrib)
                     self._receiver_info = info
                     updates[ATTR_RECEIVER_INFORMATION] = info
+                    updates[ATTR_NAME] = info.model
+                    updates[ATTR_IDENTIFIER] = info.serial
                     self.store_data()
             else:
-                _LOGGER.info(f"Ignoring zone {zone}")
+                _LOGGER.warning(f"Ignoring zone {zone}")
         except ValueError:
             _LOGGER.debug(f"Cannot decode raw message: {message}")
         if updates:
@@ -167,9 +239,9 @@ class OnkyoReceiver:
             for listener in self.listeners:
                 listener(self.data)
         if self._sync_pending:
-            _LOGGER.info(f"Received {message} whilst waiting for sync response {self._sync_command_prefix}")
+            _LOGGER.debug(f"Received {message} whilst waiting for sync response {self._sync_command_prefix}")
             if self._sync_command_prefix == message[:3]:
-                _LOGGER.info("Handled sync response")
+                _LOGGER.debug("Handled sync response")
                 self._sync_result = message
                 self._sync_pending.set()
                 return
@@ -226,44 +298,7 @@ class OnkyoReceiver:
         }
         return info
 
-    def _parse_receiver_information(self, receiver_information_xml) -> dict[str, Any]:
-        data = ET.fromstring(receiver_information_xml)
-        device = data.find('device')
-        model = device.find('model').text
-        productid = device.find('productid').text
-        serial = device.find('deviceserial').text
-        macaddress = device.find('macaddress').text
-        zones = {}
-        for zone in device.find('zonelist').findall('zone'):
-            if int(zone.attrib['value']) > 0:
-                zones[zone.attrib['id']] = {
-                    'name': zone.attrib['name'].lower(),
-                    'volmax': zone.attrib['volmax'],
-                }
 
-        sources = {}
-        for source in device.find('selectorlist').findall('selector'):
-            if int(source.attrib['value']) > 0:
-                # Assume this is a bitwise identifier for which zones support this source
-                source_zones = int(source.attrib['zone'], 16)
-                zone_ids = []
-                for zone in zones.keys():
-                    zone_id = int(zone, 16)
-                    if source_zones & (1 << (zone_id - 1)):
-                        zone_ids.append(zone)
-                sources[source.attrib['id']] = {
-                    'name': source.attrib['name'],
-                    'zones': zone_ids,
-                }
-        data = {
-            'model': model,
-            'productid': productid,
-            'serial': serial,
-            'macaddress': macaddress,
-            'zones': zones,
-            'sources': sources,
-        }
-        _LOGGER.info(f"Parsed {data}")
         return data
 
     def raw(self, command):
@@ -280,7 +315,7 @@ class OnkyoReceiver:
 
     def raw_sync(self, raw_command: str):
         """Run a raw eiscp command synchronously."""
-        _LOGGER.info(f"Sending sync command {raw_command}")
+        _LOGGER.debug(f"Sending sync command {raw_command}")
         self._sync_command_prefix = raw_command[:3]
         self._sync_pending = threading.Event()
         self._receiver.send(raw_command)
@@ -288,7 +323,7 @@ class OnkyoReceiver:
             if not self._sync_pending.wait(10):
                 raise ValueError("Timeout waiting for response")
             result_raw = self._sync_result
-            _LOGGER.info(f"Result: {result_raw}")
+            _LOGGER.debug(f"Result: {result_raw}")
             return result_raw
         finally:
             self._sync_pending = None
@@ -297,18 +332,23 @@ class OnkyoReceiver:
 
     def command_sync(self, command: str):
         """Run an eiscp command synchronously."""
-        _LOGGER.info(f"Sending sync command {command}")
+        _LOGGER.debug(f"Sending sync command {command}")
         raw_command = command_to_iscp(command)
         result_raw = self.raw_sync(raw_command)
         result = iscp_to_command(result_raw)
-        _LOGGER.info(f"Result: {result}")
+        _LOGGER.debug(f"Result: {result}")
         return result
+
+    def select_source(self, zone: ReceiverZone, source_id: int):
+        cmd = "input-selector" if zone.name == "main" else "selector"
+        raw_cmd = COMMAND_MAPPINGS[zone.name][cmd] + f"{source_id:02x}"
+        self.raw(raw_cmd)
 
     def update(self):
         """Get the latest state from the device."""
-        # some basic info
-        self.data[ATTR_NAME] = self._receiver_info["model"]
-        self.data[ATTR_IDENTIFIER] = self._receiver_info["macaddress"]
+
+        if not self._receiver_info:
+            self.command("dock.receiver-information=query")
 
         for zone in _ZONE_NAMES:
             # retrieve power information
